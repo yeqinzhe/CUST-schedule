ca508870d78b543f0d77e27d3c18ff2764e03451
yqz <yeqinzhe@mail.ru>
2025-03-06 12:41:41 +0800

    Changes

--- main.py
+++ main.py
@@ -4,7 +4,6 @@ from toICS import to_ics
 
 # 个人id F12 GetStudentBasicInfoLessonOccupy SMXSJBXXID
 PersonalId = "d85695ed-799c-418f-85ea-6e02d50a7b9b"
-# PersonalId = "cb94f4b2-3bb6-4424-834d-930c00e940b4" # 姜炫
 
 # cookie ASP.NET_SessionId
 SessionId = "f2i2isiycjtaub4oz1uuum2z"
